e3906ea74c327bd9d7cca4750f3a3323953b6a93
michelePiamarta <michele.piamarta@marconirovereto.it>
2023-12-04 16:02:16 +0100

    iniziato file per managing pulsanti start server, start client e start host

---
+++ Lan party/Assets/Player/UINetworkManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class UINetworkManager : MonoBehaviour
+{
+    [SerializeField]private Button serverHost;
+    [SerializeField]private Button hostClient;
+    [SerializeField]private Button clientServer;
+
+    
+}
