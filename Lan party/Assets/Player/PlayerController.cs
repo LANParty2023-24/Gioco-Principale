d7cfeb4b225e40b23cfefdc17521145cf2766384
michelePiamarta <michele.piamarta@marconirovereto.it>
2023-12-11 15:43:12 +0100

    aggiunta scena per lobby, modificato canva e script per managing network

--- Lan party/Assets/Player/PlayerController.cs
+++ Lan party/Assets/Player/PlayerController.cs
@@ -1,19 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Netcode;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : NetworkBehaviour
 {
     [SerializeField]private float speed;
     // Start is called before the first frame update
     void Start()
     {
-        
+        speed = 5000;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(transform.position.x + Input.GetAxis("Horizontal")*Time.fixedDeltaTime,transform.position.y + Input.GetAxis("Vertical")*Time.fixedDeltaTime);
+        if(!IsOwner){
+            return;
+        }
+        this.GetComponent<Rigidbody2D>().velocity = new Vector2(Input.GetAxis("Horizontal")*speed*Time.deltaTime,Input.GetAxis("Vertical")*speed*Time.deltaTime);
+        
     }
+
 }
--- Lan party/Assets/Player/UINetworkManager.cs
+++ Lan party/Assets/Player/UINetworkManager.cs
@@ -2,11 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Unity.Netcode;
 public class UINetworkManager : MonoBehaviour
 {
-    [SerializeField]private Button serverHost;
-    [SerializeField]private Button hostClient;
-    [SerializeField]private Button clientServer;
+    [SerializeField]private Button startHost;
+    [SerializeField]private Button startClient;
+    [SerializeField]private Button startServer;
 
-    
+    void Start(){
+        
+    }
+
+    private void Awake(){
+        startHost.onClick.AddListener(()=>{
+            NetworkManager.Singleton.StartHost();
+        });
+        startClient.onClick.AddListener(()=>{
+            NetworkManager.Singleton.StartClient();
+        });
+        startServer.onClick.AddListener(()=>{
+            NetworkManager.Singleton.StartServer();
+        });
+    }
 }
