7e28b79bc9149a64afcca08634ddc8a276fe0460
leonardo <leonardo.sparano@marconirovereto.it>
2023-12-18 15:59:56 +0100

    creato scheletro mappa palazzo, modificate collisioni e area giocabile

--- Lan party/Assets/Player/PlayerController.cs
+++ Lan party/Assets/Player/PlayerController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    
+    public Rigidbody2D rb;
+
     [SerializeField]private float speed;
     // Start is called before the first frame update
     void Start()
@@ -14,6 +17,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(transform.position.x + Input.GetAxis("Horizontal")*Time.fixedDeltaTime,transform.position.y + Input.GetAxis("Vertical")*Time.fixedDeltaTime);
+        //transform.position = new Vector3(transform.position.x + Input.GetAxis("Horizontal")*Time.fixedDeltaTime,transform.position.y + Input.GetAxis("Vertical")*Time.fixedDeltaTime);
+        rb.velocity = new Vector3(Input.GetAxis("Horizontal")*Time.deltaTime*speed,Input.GetAxis("Vertical")*Time.deltaTime*speed);
     }
 }
