62011fa2773d87af8184835f1bc7a011a4d7c8ae
michelePiamarta <michele.piamarta@marconirovereto.it>
2024-01-08 16:06:24 +0100

    aggiunta variabile network e aggiunto metodo on value changed sulla variabile network. creato gameobject per la classifica

--- Lan party/Assets/Player/PlayerController.cs
+++ Lan party/Assets/Player/PlayerController.cs
@@ -1,25 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using Unity.Netcode;
-
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
 public class PlayerController : NetworkBehaviour
 {
-    [SerializeField]private float speed;
+
+    public Text hintText;
+    private int lead = 0;
+    private NetworkVariable<int> punteggio = new NetworkVariable<int>(0, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
+    [SerializeField] private float speed;
     // Start is called before the first frame update
     void Start()
     {
         speed = 5000;
+        //GetLocalIPAddress();
+        punteggio.OnValueChanged += OnMyNetworkVariableChanged;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(!IsOwner){
+        if (!IsOwner)
+        {
             return;
         }
-        this.GetComponent<Rigidbody2D>().velocity = new Vector2(Input.GetAxis("Horizontal")*speed*Time.deltaTime,Input.GetAxis("Vertical")*speed*Time.deltaTime);
-        
+        this.GetComponent<Rigidbody2D>().velocity = new Vector2(Input.GetAxis("Horizontal") * speed * Time.deltaTime, Input.GetAxis("Vertical") * speed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            punteggio.Value++;
+        }
     }
+    private void OnMyNetworkVariableChanged(int oldValue, int newValue)
+    {
+        // Log the changes on the server
+        if (IsServer)
+        {
+            Debug.Log($"Server Log - Client {OwnerClientId}: myNetworkVariable changed from {oldValue} to {newValue}");
+        }
+        //TODO modificare la classifica su tutti i client a seconda del punteggio
+        //if(lead<newValue)
+        //    hintText.text = OwnerClientId +" : "+newValue;
+    }
+
+    //public string GetLocalIPAddress()
+    //{
+    //    var host = Dns.GetHostEntry(Dns.GetHostName());
+    //    foreach (var ip in host.AddressList)
+    //    {
+    //        
+    //        if (ip.AddressFamily == AddressFamily.InterNetwork)
+    //        {
+    //            Debug.Log(ip.ToString());
+    //            hintText.text = ip.ToString();
+    //            return ip.ToString();
+    //        }
+    //    }
+    //    throw new System.Exception("No network adapters with an IPv4 address in the system!");
+    //}
 
 }
