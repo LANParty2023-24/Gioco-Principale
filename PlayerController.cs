22894f4067a182ce0b68d9f50ebf36a3efa85fd3
Riccardo-Adami <158182544+Riccardo-Adami@users.noreply.github.com>
2024-03-12 16:12:04 +0100

    Add files via upload

---
+++ PlayerController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float speed = 5f;
+    public Rigidbody2D rb;
+    public Animator animator;
+    Vector2 movement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Vertical", movement.x);
+        animator.SetFloat("Horizontal", movement.y);
+
+        // this.transform.position = new Vector3(Mathf.Clamp(this.transform.position.x, -8.7f, 8.7f), Mathf.Clamp(this.transform.position.y, -4.5f, 3.7f), 0);
+        this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y);
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+    }
+}
